bcd788575bcb89d0ec656229fd31974f256c03da
Tejeswar Bharath <tejeswarbharath@gmail.com>
2026-07-06 21:11:01 +0530

    feat: add root App component and update Content-Security-Policy to allow unsafe-inline scripts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('consultnow-frontend');
 }
+
